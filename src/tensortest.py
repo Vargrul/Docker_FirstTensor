3519c6225f3325e12300ad25c25d79e88075f2d3
Kasper Skou Ladefoged <kasper.ladefoged@gmail.com>
2019-09-03 13:52:35 +0200

    Small cleanup

--- src/tensortest.py
+++ src/tensortest.py
@@ -17,8 +17,6 @@ import numpy as np
 import mnist_reader
 import models_mnist
 
-# mnist = tf.keras.datasets.mnist
-
 print(tf.version)
 
 x_test, y_test = mnist_reader.load_mnist('/home/ksla/projects/datasets/fashion-mnist', kind='t10k')
@@ -28,23 +26,12 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 data_train = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 data_test = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
-
-print(type(data_train))
-print(type(data_test))
-
 # data_train = data_train.map(data_prep_helper.data_prep)
 # data_test = data_test.map(data_prep_helper.data_prep)
 
 data_train = data_train.shuffle(True).batch(512).prefetch(tf.data.experimental.AUTOTUNE)
 data_test = data_test.shuffle(True).batch(512).prefetch(tf.data.experimental.AUTOTUNE)
 
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(input_shape=(28, 28)),
-#     tf.keras.layers.Dense(128, activation=tf.nn.relu),
-#     tf.keras.layers.Dense(10, activation=tf.nn.relu),
-#     tf.keras.layers.Dropout(0.2),
-#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-# ])
 model_1 = models_mnist.model_simple_dense()
 model_2 = models_mnist.model_1_conv()
 model_3 = models_mnist.model_3_conv()
